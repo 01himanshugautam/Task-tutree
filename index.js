2f47551b62da9a927dd5342802476e4efef68f37
Mahi <01himanshugautam@gmail.com>
2020-10-29 13:40:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Linked javascript file");
+
+function feedback() {
+    var a = document.getElementById('feedback');
+    a.style.display = "flex";
+}
+
+function feedShow() {
+    var a = document.getElementById('feedback');
+    a.style.display = "none";
+}
+$("#a").click(function() {
+    $("#aa").toggle();
+});
+$("#b").click(function() {
+    $("#bb").toggle();
+});
+$("#c").click(function() {
+    $("#cc").toggle();
+});
+$("#d").click(function() {
+    $("#dd").toggle();
+});
+$("#e").click(function() {
+    $("#ee").toggle();
+});
